a3d36fd900863b7d0eecfba89f7eeccb58345810
Eduardo De Nóvoa Fernández <eduardodenovoa@gmail.com>
2024-02-06 13:03:06 +0100

    Primer concepto de menú de navegación

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import logo from './images/logo.jpg'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <nav>
+        <div>
+          <img src={logo} alt="logo" className="logo"></img>  
+        </div>
+        <div>
+          <a href="#">Home</a>
+          <a href="#">MeteoIA</a>
+          <a href="#">Info</a>
+        </div>
+      </nav>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
